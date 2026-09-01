4ef4332ff3e6a4dbe549ea04c8df567b12a07059
Mart <martinqwq4@gmail.com>
2026-09-01 13:37:25 +0300

    fix: return result on success

--- src/mentor_bootcamp/main.py
+++ src/mentor_bootcamp/main.py
@@ -1,14 +1,17 @@
-def devibe_numbers(a, b):
+def divide_numbers(a, b):
     try:
         res = a / b
-        print(res)
     except ZeroDivisionError:
         print("Нельзя делить на 0")
         return None
     except TypeError:
         print(f"Одно и чисел не число: {a, b}")
         return None
+    else:
+        return res
     finally:
         print("Операция завершена")
 
-devibe_numbers(10, "два")
+print(divide_numbers(10, 2))
+print(divide_numbers(10, "два"))
+print(divide_numbers(10, 0))
